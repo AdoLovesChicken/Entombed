fd1689d365fbf6ae2f3198232a1dd31ad617b237
AdoLovesChicken <adoloveschicken@gmail.com>
2026-07-06 23:43:35 +1000

    feat(config): switch to json5

--- common/src/main/java/me/adoloveschicken/entombed/config/ConfigIO.java
+++ common/src/main/java/me/adoloveschicken/entombed/config/ConfigIO.java
@@ -1,9 +1,11 @@
 package me.adoloveschicken.entombed.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import blue.endless.jankson.Jankson;
+import blue.endless.jankson.JsonElement;
+import blue.endless.jankson.JsonObject;
+import blue.endless.jankson.JsonPrimitive;
+import blue.endless.jankson.api.SyntaxError;
+import me.adoloveschicken.entombed.config.ConfigData.DropBehavior;
 import me.adoloveschicken.entombed.Entombed;
 
 import java.io.IOException;
@@ -12,62 +14,62 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ConfigIO {
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Jankson JANKSON = Jankson.builder().build();
 
     public static void save(Path file) {
         JsonObject jsonObject = new JsonObject();
         for (Field field : ConfigData.class.getDeclaredFields()) {
             try {
                 field.setAccessible(true);
-                if (field.getType().equals(boolean.class)) // Booleans
-                    jsonObject.addProperty(field.getName(), field.getBoolean(null));
-                else if (field.getType().equals(short.class)) // Shorts (percents)
-                    jsonObject.addProperty(field.getName(), field.getShort(null));
-                else if (field.getType().equals(ConfigData.DropBehavior.class)) // DropBehaviors
-                    jsonObject.addProperty(field.getName(), field.get(null).toString());
-                else Entombed.LOGGER.warn("{} is not of known type", field.getName());
+                String name = field.getName();
+                if (field.getType().equals(boolean.class))
+                    jsonObject.put(name, JsonPrimitive.of(field.getBoolean(null)));
+                else if (field.getType().equals(short.class))
+                    jsonObject.put(name, JsonPrimitive.of((long) field.getShort(null)));
+                else if (field.getType().equals(DropBehavior.class))
+                    jsonObject.put(name, JsonPrimitive.of(field.get(null).toString()));
             } catch (Exception e) {
-                Entombed.LOGGER.error("Could not get field {} of {}", field.getName(), ConfigData.class.getName(), e);
+                Entombed.LOGGER.error("Could not get field {}", field.getName(), e);
             }
         }
         try {
-            Files.writeString(file, GSON.toJson(jsonObject));
+            Files.writeString(file, jsonObject.toJson(true, true));
         } catch (IOException e) {
-            Entombed.LOGGER.error("Could not save config file {}", file, e);
+            Entombed.LOGGER.error("Could not save config", e);
         }
     }
 
     public static void load(Path file) {
         if (!Files.exists(file)) {
-            Entombed.LOGGER.info("Config file {} does not exist", file);
             save(file);
             return;
         }
         try {
             String json = Files.readString(file);
-            JsonObject jsonObject = GSON.fromJson(json, JsonObject.class);
+            JsonObject jsonObject = JANKSON.load(json);
             for (Field field : ConfigData.class.getDeclaredFields()) {
                 try {
                     field.setAccessible(true);
-                    JsonElement jsonElement = jsonObject.get(field.getName());
-                    if (jsonObject.has(field.getName())) {
-                        if (field.getType().equals(boolean.class))
-                            field.setBoolean(null, jsonElement.getAsBoolean());
-                        else if (field.getType().equals(short.class))
-                            field.setShort(null, jsonElement.getAsShort());
-                        else if (field.getType().equals(ConfigData.DropBehavior.class))
-                            field.set(null, Enum.valueOf(ConfigData.DropBehavior.class, jsonElement.getAsString()));
-                        else Entombed.LOGGER.warn("{} is not of known type", field.getName());
+                    String name = field.getName();
+                    if (jsonObject.containsKey(name)) {
+                        JsonElement elem = jsonObject.get(name);
+                        if (field.getType().equals(boolean.class)) {
+                            boolean defaultValue = field.getBoolean(null);
+                            field.setBoolean(null, ((JsonPrimitive) elem).asBoolean(defaultValue));
+                        } else if (field.getType().equals(short.class)) {
+                            field.setShort(null, (short) ((JsonPrimitive) elem).asInt(0));
+                        } else if (field.getType().equals(DropBehavior.class)) {
+                            field.set(null, Enum.valueOf(DropBehavior.class, ((JsonPrimitive) elem).asString()));
+                        }
                     }
                 } catch (Exception e) {
-                    Entombed.LOGGER.error("Could not set field {} of {}", field.getName(), ConfigData.class.getName(), e);
+                    Entombed.LOGGER.error("Could not set field {}", field.getName(), e);
                 }
             }
             ConfigData.setItemsOnDeath(ConfigData.itemsOnDeath);
             ConfigData.setLiquidProperties(ConfigData.tombsFloatInLiquid);
-        } catch (IOException e) {
-            Entombed.LOGGER.error("Could not load config file {}", file, e);
+        } catch (IOException | SyntaxError e) {
+            Entombed.LOGGER.error("Could not load config", e);
         }
-
     }
 }
