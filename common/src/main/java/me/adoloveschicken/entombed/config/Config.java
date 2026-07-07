d3a7ab5feae3f705087605933016a96c380cef73
AdoLovesChicken <adoloveschicken@gmail.com>
2026-07-07 12:45:21 +1000

    Config migration JSON -> JSON5

--- common/src/main/java/me/adoloveschicken/entombed/config/Config.java
+++ common/src/main/java/me/adoloveschicken/entombed/config/Config.java
@@ -1,17 +1,31 @@
 package me.adoloveschicken.entombed.config;
 
+import me.adoloveschicken.entombed.Entombed;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class Config {
     private static Path configDir;
 
     public static void load(Path dir) {
         configDir = dir;
-        ConfigIO.load(configDir.resolve("entombed.json"));
+        Path json = configDir.resolve("entombed.json");
+        Path json5 = configDir.resolve("entombed.json5");
+        if (Files.exists(json) && !Files.exists(json5)) {
+            try {
+                Files.move(json, json5);
+            } catch (IOException e) {
+                Entombed.LOGGER.warn("Could not migrate old config", e);
+            }
+        }
+        ConfigIO.load(configDir.resolve(json5));
     }
 
     public static void save() {
-        ConfigIO.save(configDir.resolve("entombed.json"));
+        ConfigIO.save(configDir.resolve("entombed.json5"));
     }
 
 }
