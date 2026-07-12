b394bd4d8f91b9900b0bfa0edcb82aa181740808
AdoLovesChicken <adoloveschicken@gmail.com>
2026-07-12 13:50:12 +1000

    Update breaking behavior

--- common/src/main/java/me/adoloveschicken/entombed/block/GravestoneBlock.java
+++ common/src/main/java/me/adoloveschicken/entombed/block/GravestoneBlock.java
@@ -98,11 +98,7 @@ public class GravestoneBlock extends BaseEntityBlock {
     public BlockState playerWillDestroy(Level level, BlockPos pos, BlockState state, Player player) {
         if (!level.isClientSide && level.getBlockEntity(pos) instanceof GravestoneBlockEntity grave) {
             if (grave.getOwnerUUID() != null && grave.getGraveID() != null) {
-                if (ConfigData.tombsCanBeBrokenDirectly) {
-                    grave.restoreAll(player);
-                    return super.playerWillDestroy(level, pos, state, player);
-                }
-                return state;
+                grave.restoreAll(player);
             }
         }
         return super.playerWillDestroy(level, pos, state, player);
@@ -143,7 +139,11 @@ public class GravestoneBlock extends BaseEntityBlock {
 
     @Override
     protected float getDestroyProgress(BlockState state, Player player, BlockGetter level, BlockPos pos) {
-        if (ConfigData.tombsCanBeBrokenDirectly) return 0;
+        if (level.getBlockEntity(pos) instanceof GravestoneBlockEntity grave) {
+            if (!ConfigData.tombsCanBeBrokenDirectly && !player.getUUID().equals(grave.getOwnerUUID())) {
+                return 0;
+            }
+        }
         return super.getDestroyProgress(state, player, level, pos);
     }
 
